3521c863ccae9eff8e66d26903b873c0997d4e15
unknown <marley549@icloud.com>
2019-04-24 20:00:28 -0500

    worked on falling circles

--- tester_mo.py
+++ tester_mo.py
@@ -2,27 +2,53 @@ import pygame
 import time 
 WIDTH = 1000
 HEIGHT = 1000
-
+C_SPEED = 2
 pygame.init()
 
-screen = pygame.display.set_mode((WIDTH,WIDTH))
+background_image = pygame.image.load("plain_bg.jpg")
+back_pos = [0,0]
+
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
 done = False
 
+# LOCATION OF COLUMN ONE
+x1 = 400
+y1 = 50
 
+#LOCATION OF COLUMN TWO
+x2 = 450
+y2 = 50
 
-def circle(color,x,y,width):
-    pygame.draw.circle(screen,color,(x,y),width)
+#LOCATION OF COLUMN THREE
+x3 = 500
+y3 = 50
 
+#LOCATION OF COLUMN FOUR 
+x4 = 550
+y4 = 50
+
+def circle(color,size):
+    pygame.draw.circle(screen,color,size,10)
 while not done:
+    screen.blit(background_image,back_pos )
+    
+    circle((0,0,255),(x1,y1))
+    circle((0,0,255),(x2,y2))
+    circle((0,0,255),(x3,y3))
+    circle((0,0,255),(x4,y4))
+    y1 = y1 + C_SPEED
+    if y1 > HEIGHT:
+        y1 = -1
 
-    for i in range(5):
-        pygame.display.init
-        circle((255,0,0),500-i,500-i,50)
-   
-
-
-
-
-
+    
+    
+    
     pygame.display.flip()
+        
+
+
+
+
+
+   
     
