e9b962cd9052498ff4e56caedddf1adda773e542
Andres Flores <andres06037@gmail.com>
2019-05-13 13:46:51 -0500

    test1

--- sound_test.py
+++ sound_test.py
@@ -7,18 +7,22 @@ e = 329.63
 d = 293.66
 c = 261.63
 
+# instrument = grand_piano
+
 wait_time = 0.5
 
 pygame.midi.init()
 
-player = pygame.midi.get_default_output_id()
+pygame.midi.Output(pygame.midi.get_default_output_id())
 
-player.set_instrument(1)
+pygame.midi.Output.set_instrument(22)
 
-player.note_on(c)
+pygame.midi.Output.note_on(c)
 time.sleep(wait_time)
-player.note_off(c)
+pygame.midi.Output.note_off(c)
 
-player.note_on(c)
+pygame.midi.Output.note_on(c)
 time.sleep(wait_time)
-player.note_off(c)
+pygame.midi.Output.note_off(c)
+
+pygame.midi.quit()
