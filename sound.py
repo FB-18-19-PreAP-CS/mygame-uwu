937d5e893ff82d3a4887d42820eb6d38e9034563
Marlena Osgood <marley549@icloud.com>
2019-05-13 14:09:30 -0500

    also wack

--- sound.py
+++ sound.py
@@ -18,7 +18,7 @@ print(pygame.midi.get_device_info(1))
 print(pygame.midi.get_default_output_id())
 
 
-pygame.midi.Output.set_instrument(22,grand_piano)
+pygame.midi.Output.set_instrument(22)
 
 pygame.midi.Output.note_on(c,127)
 time.sleep(wait_time)
