211adb1b09933543debfbb05a2f8477ba18ba12d
Marlena Osgood <marley549@icloud.com>
2019-05-14 13:45:20 -0500

    working on sound

--- tester_mo.py
+++ tester_mo.py
@@ -1,10 +1,13 @@
 import pygame
 import time 
+import pygame_functions
 WIDTH = 1000
 HEIGHT = 1000
-C_SPEED = 2
-pygame.init()
 
+
+
+pygame.init()
+screenRefresh = True
 background_image = pygame.image.load("plain_bg.jpg")
 back_pos = [0,0]
 
@@ -12,82 +15,184 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT))
 done = False
 
 class Button(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,loc):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("blue_button-1-1.png.png")
+        self.loc = loc
+        self.is_visible = True
+    def blitme(self):
+        if self.is_visible == True:
+            screen.blit(self.image,self.loc)
+    def move_y(self):
+        self.loc[1] = self.loc[1]+5
+        
 
+class Blank(pygame.sprite.Sprite):
+    def __init__(self,loc):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("blank.png")
+        self.loc = loc
+        self.is_visible = True
+    def blitme(self):
+        if self.is_visible == True:
+            screen.blit(self.image,self.loc)
+    def move_y(self):
+        self.loc[1] = self.loc[1]+5
+       
 class Target():
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("target-1.png.png")
+    
 
-
-# LOCATION OF COLUMN ONE
-x1 = 400
-y1 = 50
-
-#LOCATION OF COLUMN TWO
-x2 = 450
-y2 = 50
-
-#LOCATION OF COLUMN THREE
-x3 = 500
-y3 = 50
-
-#LOCATION OF COLUMN FOUR 
-x4 = 550
-y4 = 50
 
 #CREATES INITIAL COLUMNS OF FALLING BUTTONS
 
+
+column_a = ['-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','x','-','-','-','-','-','-','-','x','-','-','-','-','-','-','-','-','x','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','x']
+column_s = ['-','x','x','-','-','-','-','-','-','-','-','-','-','x','x','-','-','-','-','-','-','x','x','-','-','-','-','-','-','-','x','x','-','-','x','x','-','-','-','-','-','-','-','-','-','-','x','x','-']
+column_d = ['-','-','-','x','x','-','-','x','-','-','-','x','x','-','-','-','-','-','-','x','x','-','-','-','-','-','-','-','x','x','-','-','-','-','-','-','x','x','-','-','x','-','-','-','x','x','-','-','-']
+column_f = ['-','-','-','-','-','x','x','-','-','x','x','-','-','-','-','-','-','x','x','-','-','-','-','-','-','x','x','-','-','-','-','-','-','-','-','-','-','-','x','x','-','-','x','x','-','-','-','-','-']
+# column_a.reverse()
+# column_s.reverse()
+# column_d.reverse()
+# column_f.reverse()
+ 
+def beatmap():
+    buttons_a = []
+    buttons_s = []
+    buttons_d = []
+    buttons_f = []
+    row = 0
+    spacing = 100
+    for space_a,space_s,space_d,space_f in zip(column_a,column_s,column_d,column_f):
+
+        if space_a == "-":
+            blank_a = Blank([400,20 - row * spacing])
+        
+            buttons_a.append(blank_a)
+            
+        if space_a == "x":
+            butt_a = Button([400,20 - row * spacing])
+            
+            buttons_a.append(butt_a)
+            
+        if space_s == "-":
+            blank_s = Blank([450,20- row * spacing])
+            
+            buttons_s.append(blank_s)
+
+
+        if space_s == "x":
+            butt_s = Button([450,20- row * spacing])
+            
+            buttons_s.append(butt_s)
+
+
+        if space_d == "-":
+            blank_d = Blank([500,20- row * spacing])
+            
+            buttons_d.append(blank_d)
+
+        if space_d == "x":
+            butt_d = Button([500,20- row * spacing])
+            
+            buttons_d.append(butt_d)
+
+        if space_f == "-":
+            blank_f = Blank([550,20- row * spacing])
+            
+            buttons_f.append(blank_f)
+
+        if space_f == "x":
+            butt_f = Button([550,20- row * spacing])
+            
+            buttons_f.append(butt_f)
+        
+        row += 1
     
+    return [buttons_a,buttons_s,buttons_d,buttons_f]
+bm = beatmap()
+def show_buttons(bm):
+    
+   
+    for ele in bm[0]:
+        if type(ele) == Blank:
+            print('blank')
+        elif type(ele) == Button:
+            print('button')
+        
+        ele.blitme()
+        ele.move_y()
+
+    for ele in bm[1]:
+        
+        ele.blitme()
+        ele.move_y()
+    for ele in bm[2]:
+        
+        ele.blitme()
+        ele.move_y()
+    for ele in bm[3]:
+        
+        ele.blitme()
+        ele.move_y()
+
+
+
+
+pygame.mixer.init()
+first_song = pygame.mixer.music.load("tt_lit.ogg")
+pygame.mixer.music.play()
+pygame.event.wait()
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
 
-    
-   
-    screen.blit(background_image,back_pos )
-    #CREATES TARGET
-    t=Target()
-    screen.blit(t.image,(375,400))
-
-    #CREATES 4 COLUMNS
-    button1 = Button()
-    button2 = Button()
-
-    button3 = Button()
-    button4 = Button()
-    screen.blit(button1.image,(x1,y1))
-    y1 = y1 + C_SPEED
-    if y1 > HEIGHT:
-        y1 = -1
-    screen.blit(button1.image,(x2,y2))
-    y2 = y2 + C_SPEED
-    if y2 > HEIGHT:
-        y2 = -1
-    screen.blit(button1.image,(x3,y3))
-    y3 = y3 + C_SPEED
-    if y3 > HEIGHT:
-        y3 = -1
-    screen.blit(button1.image,(x4,y4))
-    y4 = y4+ C_SPEED
-    if y4 > HEIGHT:
-        y4 = -1
-
-    
-
-    
-    
-    
-    pygame.display.flip()
         
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                while not type(bm[0][0]) == Button:
+                    bm[0] = bm[0][1:]
+                bm[0][0].is_visible = False
+                bm[0] = bm[0][1:]
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_s:
+                while not type(bm[1][0]) == Button:
+                    bm[1]=bm[1][1:]
+                bm[1][0].is_visible = False
+                bm[1]=bm[1][1:]
+
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_d:
+        #         if type(bm[2][0]) == Button:
+        #             bm[2][0].is_visible = False
+        # bm[2] = bm[2][1:]
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                while not type(bm[2][0]) == Button:
+                    bm[2] = bm[2][1:]
+                bm[2][0].is_visible = False
+                bm[2] = bm[2][1:]
 
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_f:
+                while not type(bm[3][0]) == Button:
+                    bm[3] = bm[3][1:]
+                bm[3][0].is_visible = False
+                bm[3] = bm[3][1:]
+            
+    screen.blit(background_image,back_pos )
 
 
+    t=Target()
+    screen.blit(t.image,(375,700))
 
-   
-    
+    show_buttons(bm)
+ 
+    pygame.display.flip()
